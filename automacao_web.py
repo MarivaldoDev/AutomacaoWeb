5bc848cdbf9909104924c0965345deb4fa2eca72
MarivaldoProgramador <126726979+MarivaldoProgramador@users.noreply.github.com>
2023-07-11 16:30:29 -0300

    Update automacao_web.py

--- automacao_web.py
+++ automacao_web.py
@@ -1,3 +1,5 @@
+# BAIXE O ARQUIVO "Produtos.xlsx" E COLOQUE NO MESMO LOCAL DO CÓDIGO, PARA O MESMO FUNCIONAR CORRETAMENTE
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
