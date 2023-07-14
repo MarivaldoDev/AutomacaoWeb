b74401bb57514dd8437cb1d3e8bd0b6a96507836
Marivaldo Pedro <126726979+MarivaldoDev@users.noreply.github.com>
2023-07-14 14:54:14 -0300

    Update automacao_web.py

--- automacao_web.py
+++ automacao_web.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 import pandas as pd
 
 #  PEGANDO COTAÇÃO DO DÓLAR
